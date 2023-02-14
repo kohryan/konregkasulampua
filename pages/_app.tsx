ed231c3d879cab742affe4da3fa8d86824d23779
Ryan W. Januardi <100107440+kohryan@users.noreply.github.com>
2023-02-14 15:48:12 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,8 @@
 import 'tailwindcss/tailwind.css'
 import "../custom.css";
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
-import { SessionContextProvider } from '@supabase/auth-helpers-react'
-import { useState } from 'react'
 
 export default function App({ Component, pageProps }) {
-  const [supabase] = useState(() => createBrowserSupabaseClient())
-
   return (
-    <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
       <Component {...pageProps} />
-    </SessionContextProvider>
   )
 }

1af06a655bdec9cd571a4aeb737883af967f9d00
Ryan W. Januardi <100107440+kohryan@users.noreply.github.com>
2023-03-13 16:28:13 +0800

    roudo

--- components/konreg/Kedua.tsx
+++ components/konreg/Kedua.tsx
@@ -150,12 +150,12 @@ export function HariKedua () {
                     Strategi Penguatan Konektivitas dalam Mewujudkan Pertumbuhan Ekonomi Inklusif Kasulampua </h5>
                     <div className="mt-3 flex items-center space-x-4">
                       <div className="flex-shrink-0">
-                       <Image className="rounded-full" width="64" height="64" src="/people/blank.png" alt="Blank" />
+                       <Image className="rounded-full" width="64" height="64" src="/people/roudo.png" alt="Blank" />
                       </div>
                         <dl className="ml-3 text-sm font-medium leading-4 text-left whitespace-no-wrap">
                           <dd className="mb-1 text-md font-bold text-gray-400 dark:text-gray-300">Narasumber:</dd>
-                          <dd className="text-lg text-gray-900 dark:text-white">TBD</dd>
-                          <p className="text-sm text-gray-400 dark:text-gray-300">Bappenas RI</p>
+                          <dd className="text-lg text-gray-900 dark:text-white">Mohammad Roudo, ST, MPP, Ph.D</dd>
+                          <p className="text-sm text-gray-400 dark:text-gray-300">Plt. Direktur Regional II, Kedeputian Bidang Pengembangan Regional Bappenas RI</p>
                         </dl>
                     </div>
 
